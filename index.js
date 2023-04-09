253698b09d718791a09e7fd2fc9e26d795e3d104
Pankumi <ipankumi@gmail.com>
2023-04-09 18:15:44 +0300

    part 4

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-
 // file "index.js":
 
-const yargs = require('yargs/yargs'); // для зручного парса аргументів командного рядка
+const argv = require("yargs").argv; // для зручного парса аргументів командного рядка
 
-const { listContacts, getContactById, removeContact, addContact } = require("./contacts");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 
-// listContacts()
-// .then( data => console.log('index.js  listContacts >>', data) )
+console.log("process.argv >>", process.argv); // містить масив з аргументів які були передані при запуску процесу
+console.log("argv >>", argv);
 
-// getContactById('Z5sbDlS7pCzNsnAHLtDJd7')
-// .then( data => console.log('index.js getContactById >>', data) )
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts().then((data) =>
+        console.log("index.js  listContacts >>", data)
+      ); // cli: node index.js --action list
+      break;
 
-// removeContact('Z5sbDlS7pCzNsnAHLtDJd7')
-// .then( data => console.log('index.js removeContact >>', data) )
+    case "get":
+      getContactById(id).then((data) =>
+        console.log("index.js getContactById >>", data)
+      ); // cli: node index.js --action get --id rsKkOQUi80UsgVPCcLZZW
+      break;
 
-addContact('name', 'email', 'phone')
-.then( data => console.log('index.js addContact >>', data) )
+    case "add":
+      addContact(name, email, phone).then((data) =>
+        console.log("index.js addContact >>", data)
+      ); // cli: node index.js --action add --name Vasil --email Vasil@gmail.com --phone 0123456789
+      break;
 
+    case "remove":
+      removeContact(id).then((data) =>
+        console.log("index.js removeContact >>", data)
+      ); // cli: node index.js --action remove --id 0511e36e7ee6fe1362fd
+      break;
 
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
 
-
-
+invokeAction(argv);
